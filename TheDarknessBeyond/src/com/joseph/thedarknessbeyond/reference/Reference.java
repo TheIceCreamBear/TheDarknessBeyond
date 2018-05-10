ac35922a6c0b346329259f527ad720678dbd11ca
Joseph <kartingman25@gmail.com>
2018-05-10 11:59:13 -0400

    Add some font Util methods

--- TheDarknessBeyond/src/com/joseph/thedarknessbeyond/reference/Reference.java
+++ TheDarknessBeyond/src/com/joseph/thedarknessbeyond/reference/Reference.java
@@ -2,6 +2,9 @@ package com.joseph.thedarknessbeyond.reference;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.font.TextAttribute;
+import java.util.Hashtable;
+import java.util.Map;
 
 /**
  * Commonly used Objects or primitives stored in one location for quick Refrence
@@ -19,7 +22,18 @@ public class Reference {
 	}
 	
 	public static class Fonts {
+		private static Map<TextAttribute, Object> map;
+		public static void init() {		
+			System.out.println("fasskdfg");
+			map = new Hashtable<TextAttribute, Object>();
+			map.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+			DEFAULT_UNDERLINED_FONT = DEFAULT_FONT.deriveFont(map);
+			SCALED_UP_UNDERLINED_FONT = SCALED_UP_FONT.deriveFont(map);
+		}
+		
 		public static final Font DEFAULT_FONT = new Font("Consolas", 0, 20);
 		public static final Font SCALED_UP_FONT = new Font("Consolas", 0, 40);
+		public static Font DEFAULT_UNDERLINED_FONT;
+		public static Font SCALED_UP_UNDERLINED_FONT;
 	}
 }
--- TheDarknessBeyond/src/com/joseph/thedarknessbeyond/reference/ScreenRefrence.java
+++ TheDarknessBeyond/src/com/joseph/thedarknessbeyond/reference/ScreenRefrence.java
@@ -1,5 +1,6 @@
 package com.joseph.thedarknessbeyond.reference;
 
+import java.awt.Font;
 import java.awt.Point;
 import java.awt.Toolkit;
 
@@ -20,13 +21,24 @@ public class ScreenRefrence {
 //	public static final int height = 800;
 	
 	public static void doScreenCalc() {
-//		System.err.println(Toolkit.getDefaultToolkit().getScreenResolution());
-//		System.err.println(Toolkit.getDefaultToolkit().getScreenSize());
+		if (Reference.DEBUG_MODE) {
+			System.err.println(Toolkit.getDefaultToolkit().getScreenResolution());
+			System.err.println(Toolkit.getDefaultToolkit().getScreenSize());
+		}
+		
 		if (Toolkit.getDefaultToolkit().getScreenResolution() > 100) {
 			scale = 2;
 		} else {
 			scale = 1;
 		}
+	}
+	
+	public static Font getTheFont() {
+		return (scale == 2) ? Reference.Fonts.SCALED_UP_FONT : Reference.Fonts.DEFAULT_FONT;
+	}
+	
+	public static Font getUnderlinedFont() {
+		return (scale == 2) ? Reference.Fonts.SCALED_UP_UNDERLINED_FONT : Reference.Fonts.DEFAULT_UNDERLINED_FONT;
 	}
 	
 	public static Point getMouseLocation() {
