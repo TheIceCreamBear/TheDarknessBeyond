0686261e1e0e8871542a984b594305be2cdea499
Joseph <kartingman25@gmail.com>
2018-05-31 23:34:46 -0400

    Conflict Resolution

--- TheDarknessBeyond/src/com/joseph/thedarknessbeyond/gui/screens/RoomScreen.java
+++ TheDarknessBeyond/src/com/joseph/thedarknessbeyond/gui/screens/RoomScreen.java
@@ -9,6 +9,26 @@ public class RoomScreen extends Screen {
 
 	public RoomScreen(int x, int y, int width, int height) {
 		super(x, y, width, height, true);
+		buttons = new GenericBuildButton[14];
+		EnumBuilding[] values = EnumBuilding.values();
+		buttons[0] = new GenericBuildButton(x + 100, y + 150, EnumBuilding.Hut);
+		buttons[1] = new GenericBuildButton(x + 100, y + 225, EnumBuilding.Storage);
+		buttons[2] = new GenericBuildButton(x + 100, y + 300, EnumBuilding.Armory);
+		buttons[3] = new GenericBuildButton(x + 100, y + 375, EnumBuilding.Workshop);
+		buttons[4] = new GenericBuildButton(x + 100, y + 450, EnumBuilding.Forge);
+		buttons[5] = new GenericBuildButton(x + 100, y + 525, EnumBuilding.Blacksmith);
+		buttons[6] = new GenericBuildButton(x + 100, y + 600, EnumBuilding.Charcuterie);
+		buttons[7] = new GenericBuildButton(x + 400, y + 150, EnumBuilding.Garden);
+		buttons[8] = new GenericBuildButton(x + 400, y + 225, EnumBuilding.Quarry);
+		buttons[9] = new GenericBuildButton(x + 400, y + 300, EnumBuilding.WaterWorks);
+		buttons[10] = new GenericBuildButton(x + 400, y + 375, EnumBuilding.LumberMill);
+		buttons[11] = new GenericBuildButton(x + 400, y + 450, EnumBuilding.GuardPost);
+		buttons[12] = new GenericBuildButton(x + 400, y + 525, EnumBuilding.Barricades);
+		buttons[13] = new GenericBuildButton(x + 400, y + 600, EnumBuilding.Cathedral);
+		
+		for (int i = 0; i < buttons.length; i++) {
+			GameEngine.getInstance().addButton(this.buttons[i]);
+		}
 	}
 
 	@Override
